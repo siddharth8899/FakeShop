7c04220876f0ae969a48b86b8bcd9b75220a87ab
siddharth <siddharth.jain@peopleintereactive.in>
2021-09-04 20:03:49 +0530

    scrolls added

--- src/Products.js
+++ src/Products.js
@@ -1,4 +1,5 @@
-import {useEffect} from 'react';
+import { TextField } from '@material-ui/core';
+import {useEffect, useRef} from 'react';
 import { useDispatch , useSelector} from 'react-redux';
 import { fetchProducts } from "./doShopAction/apiGetProducts";
 import Product from './Product';
@@ -8,15 +9,50 @@ function Products(){
 const dispatch = useDispatch();
 const State = useSelector(state => state.reducer);
 const {data: products, loading} = State;
+const commentSection = useRef(null);
 
 useEffect(() => {
  dispatch(fetchProducts())
   }, [])
 
+  const scrollToTop = () => {
+    return window.scrollTo({
+      top: 0,  //issue
+      behavior: 'smooth'
+    });
+  };
+
+  const scrollToBottom = () => {
+    return window.scrollTo({
+      top: document.documentElement.scrollHeight,  //XX bottom: 0 XX  //issue
+      behavior: 'smooth'
+    });
+  };
+
+  const scrollToComment = () => {
+    return window.scrollTo({
+        top: commentSection.current.offsetTop,
+        behavior: 'smooth'
+    })
+  }
+
 return (
+  <>
+  <br />
+  <hr />
+  <button onClick={scrollToBottom}> Go to bottom </button>
+  <button onClick={scrollToComment}> Comment Form </button>
+  <hr />
+  {
   loading ? <h4> Loading ... </h4> : products.map((item, index) => {
       return <div key= {index}><Product item= {item}/></div>
     })
+  }
+  <TextField 
+  ref = {commentSection}
+  ></TextField>
+  <button onClick={scrollToTop}> ^ </button>
+  </>
 )
 }
 
